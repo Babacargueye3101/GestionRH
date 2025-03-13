8539d9780c6cdbcc302b9750a22632870e4fe7c3
Babacar Gueye Root <babacargueye3101@gmail.com>
2025-03-13 15:45:27 +0000

    redesign du home component

---
+++ src/app/services/home.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HomeService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  // Récupérer tous les Products
+  getAllProduct(): Observable<any[]> {
+    return this.http.get<any>(this.apiUrl+"/public/all_products");
+  }
+
+  // Récupérer un Product par category
+  getProductByCategory(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/public/category/${id}/products`);
+  }
+
+  getAllCategory(){
+    return this.http.get(this.apiUrl+"/public/all_categories");
+  }
+}
