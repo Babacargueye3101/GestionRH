c20cb0a0f0a31304a5ea42a4f02f9179537aee55
Babacar Gueye Root <babacargueye3101@gmail.com>
2025-05-22 21:57:11 +0000

    add button csv

--- src/app/pages/admin/ventes/ventes.component.ts
+++ src/app/pages/admin/ventes/ventes.component.ts
@@ -6,15 +6,44 @@ import { Shop } from 'src/app/models/shop';
 import { ActivatedRoute } from '@angular/router';
 import { PaymentMethodeComponent } from './payment-methode/payment-methode.component';
 import { CanalComponent } from './canal/canal.component';
+import { ListVentesComponent } from './list-ventes/list-ventes.component';
+import { ViewChild } from '@angular/core';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-ventes',
   templateUrl: './ventes.component.html',
   styleUrls: ['./ventes.component.scss']
 })
-export class VentesComponent{
+export class VentesComponent implements OnInit {
+  @ViewChild(ListVentesComponent) listVentes!: ListVentesComponent;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog) {
+    // Ajouter les imports nécessaires
+  }
+
+  ngOnInit() {
+    // Initialisation du composant
+  }
+
+  exportToCsv() {
+    if (this.listVentes) {
+      const data = this.listVentes.ventes;
+      const headers = Object.keys(data[0]);
+      const csvContent = [
+        headers.join(','),
+        ...data.map(row => headers.map(header => {
+          const cell = row[header];
+          return typeof cell === 'string' ? cell.replace(/"/g, '""') : cell;
+        }).join(','))
+      ].join('\n');
+
+      const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+      const currentDate = new Date().toISOString().split('T')[0];
+      saveAs(blob, `ventes_${currentDate}.csv`);
+    }
+  }
+
   openCreateVenteModal() {
     const dialogRef = this.dialog.open(CreateVenteComponent, {
       width: '600px'
@@ -26,6 +55,7 @@ export class VentesComponent{
       }
     });
   }
+
   openCreatePaymentMethodeModal() {
     const dialogRef = this.dialog.open(PaymentMethodeComponent, {
       width: '600px'
@@ -37,6 +67,7 @@ export class VentesComponent{
       }
     });
   }
+
   openCreateCanalModal() {
     const dialogRef = this.dialog.open(CanalComponent, {
       width: '600px'
